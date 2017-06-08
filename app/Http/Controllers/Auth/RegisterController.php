c3a7c978824be579375474b80f012bdb5107e9be
Unknown <johancamilosuarezcampos@gmail.com>
2017-06-08 17:41:35 -0500

    Configurando el Proyecto
    https://www.youtube.com/watch?v=YunVjXjKFzg&index=4&list=PLIddmSRJEJ0u-5Nv2k6W8Vhe0wUP_7H5W

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Cinema\Http\Controllers\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use Cinema\User;
+use Cinema\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
